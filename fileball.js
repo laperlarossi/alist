16fe7158130d1fa2c15e84cadf42dee16c2758a3
laperlarossi <valentinogianvitorossi@gmail.com>
2023-02-16 21:25:57 +0800

    Update fileball.js

--- fileball.js
+++ fileball.js
@@ -18,7 +18,7 @@ force-http-engine-hosts = %APPEND% *.example.com:0
 //要开mpv的话必须添加
 
 [Script]
-云盘挂载 = type=http-request,pattern=^http:\/\/(aliyun|quark|pikpak)\.example\.com,requires-body=1,script-path=https://raw.githubusercontent.com/githubdulong/Script/master/fileball.js
+云盘挂载 = type=http-request,pattern=^http:\/\/(aliyun|quark|pikpak)\.example\.com,requires-body=1,script-path= https://raw.githubusercontent.com/laperlarossi/alist/main/fileball.js
 
 Fileball挂载图标：https://raw.githubusercontent.com/githubdulong/Script/master/Images/Fileball.json
 
